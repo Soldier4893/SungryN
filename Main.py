bf99f772a9308ee9aea92493b58d74d0ffac5513
Soldier4893 <58051347+Soldier4893@users.noreply.github.com>
2023-03-11 16:31:28 -0600

    add left right direction and music

--- Main.py
+++ Main.py
@@ -1,35 +1,36 @@
-import pygame
+import pygame as pg
 import time
 import numpy as np
 from Player import Player
 from Drink import Drink
 
-# Initialize pygame
-pygame.init()
-win = pygame.display.set_mode([700, 700])  # Set up the drawing window
-pygame.display.set_caption("HUNGRY SN")  # Set caption
+# Initialize pg
+pg.init()
+win = pg.display.set_mode([700, 700])  # Set up the drawing window
+pg.display.set_caption("SUNGRYN")  # Set caption
 
 # Images
-snImg = pygame.image.load('./assets/sn2.png')
-bobaImg = pygame.image.load('./assets/boba.png')
-coffeeImg = pygame.image.load('./assets/coffee.png')
+snImg = pg.image.load('./assets/sn2.png')
+bobaImg = pg.image.load('./assets/boba.png')
+coffeeImg = pg.image.load('./assets/coffee.png')
 
 # Game variables
-clock = pygame.time.Clock()
+clock = pg.time.Clock()
 player = Player(350, 600, 40, 60, 10, snImg)
 drinks_lst = []  # This will be used to store all the drinks in the game
 start_time = time.time()
-font = pygame.font.SysFont('comicsans', 30, bold=True, italic=False)
+font = pg.font.SysFont('comicsans', 30, bold=True, italic=False)
 SCORE = 0
 LIVES = 3
 DIFFICULTY = 1
+DIRECTION = 0
 
 
-def redrawGameWindow(win):
+def redrawGameWindow(win, direction):
     win.fill((255,255,255))  # Fills the screen with white (Refreshes the screen)
 
     # Drawing the player and drink objects to the screen
-    player.draw(win)
+    player.draw(win, direction)
     for drink in drinks_lst:  
         drink.draw(win)
 
@@ -44,13 +45,22 @@ def redrawGameWindow(win):
     if LIVES == 0:
         win.blit(text, (250, 350))  
 
-    pygame.display.update() # This updates the screen so we can see our SN
+    pg.display.update() # This updates the screen so we can see our SN
 
 if __name__ == "__main__":    
+    
+    direction = 0
+    
+    # Music
+    pg.mixer.init()
+    pg.mixer.music.load("./assets/score.mp3")
+    pg.mixer.music.set_volume(0.3)
+    pg.mixer.music.play()
+    
     # Run until user runs out of lives
     run = True
     while run:
-        # pygame.time.delay(100) # This will delay the game the given amount of milliseconds. In our casee 0.1 seconds will be the delay
+        # pg.time.delay(100) # This will delay the game the given amount of milliseconds. In our casee 0.1 seconds will be the delay
         clock.tick(60)  # This will set the FPS to 60
 
         # If the player runs out of lives, remove all the drinks from the game
@@ -65,18 +75,20 @@ if __name__ == "__main__":
                 DIFFICULTY += 0.2
             
 
-        for event in pygame.event.get():  # This will loop through a list of any keyboard or mouse events.
-            if event.type == pygame.QUIT: # Checks if the red button in the corner of the window is clicked
+        for event in pg.event.get():  # This will loop through a list of any keyboard or mouse events.
+            if event.type == pg.QUIT: # Checks if the red button in the corner of the window is clicked
                 run = False  # Ends the game loop
         
-        keys = pygame.key.get_pressed()  # This will return a list of all the keys that are currently being pressed
+        keys = pg.key.get_pressed()  # This will return a list of all the keys that are currently being pressed
         
         # If the left arrow key is pressed and the player is not at the left edge of the screen
-        if keys[pygame.K_LEFT] and player.x > player.vel:
+        if keys[pg.K_LEFT] and player.x > player.vel:
+            direction = 0
             player.x -= player.vel
 
         # If the right arrow key is pressed and the player is not at the right edge of the screen
-        if keys[pygame.K_RIGHT] and player.x < 680 - player.width - player.vel:
+        if keys[pg.K_RIGHT] and player.x < 680 - player.width - player.vel:
+            direction = 1
             player.x += player.vel
         
         # Code to move the drinks down the screen
@@ -91,6 +103,6 @@ if __name__ == "__main__":
                     drinks_lst.remove(drink)
                     SCORE += 1
 
-        redrawGameWindow(win)  # This will redraw the game window
+        redrawGameWindow(win, direction)  # This will redraw the game window
 
-    pygame.quit()  # If we exit the loop this will execute and close our game
+    pg.quit()  # If we exit the loop this will execute and close our game
--- Player.py
+++ Player.py
@@ -1,3 +1,4 @@
+import pygame as pg
 class Player(object):
     def __init__(self, x, y, width, height, vel, img):
         self.x = x
@@ -7,6 +8,10 @@ class Player(object):
         self.vel = vel
         self.img = img
         self.r = 64
+        self.img2 = pg.transform.flip(self.img, True, False)
 
-    def draw(self, win):
-        win.blit(self.img, (self.x, self.y))
+    def draw(self, win, direction):
+        if direction == 0:
+            win.blit(self.img, (self.x, self.y))
+        else:
+            win.blit(self.img2, (self.x, self.y))
